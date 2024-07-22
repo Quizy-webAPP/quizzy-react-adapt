c2d4701e2b26cbd7fb6b614e4c5a0069b7f0616b
Joeltabe <joeltabe3@gmail.com>
2024-07-22 23:10:20 +0100

    "Changed multer upload from single file to multiple files"

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage }).single('file');
+const upload = multer({ storage });
 
 exports.createQuestion = (req, res) => {
   upload(req, res, async (err) => {

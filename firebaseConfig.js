e469e691e8d2da259abb292dca338e6c770d35f4
Joeltabe <joeltabe3@gmail.com>
2024-07-26 16:53:20 +0100

    "Updated .gitignore to include additional file and modified firebaseConfig.js to reference service account file from local directory instead of Downloads folder"

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // firebaseConfig.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../Downloads/school-management-e7a71-firebase-adminsdk-8dn9n-dbc0b4ca7b.json');
+const serviceAccount = require('./school-management-e7a71-firebase-adminsdk-8dn9n-dbc0b4ca7b.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

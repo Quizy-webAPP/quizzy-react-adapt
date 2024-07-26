ca701a9256d1fda56d2f983bb56f6296332b3413
Joeltabe <joeltabe3@gmail.com>
2024-07-26 16:47:11 +0100

    remove senstve data

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+const admin = require('firebase-admin');
+const serviceAccount = require('../../../Downloads/school-management-e7a71-firebase-adminsdk-8dn9n-dbc0b4ca7b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'gs://school-management-e7a71.appspot.com'
+});
+
+const bucket = admin.storage().bucket();
+module.exports = bucket;
